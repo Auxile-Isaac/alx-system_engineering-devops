b24199c73b5c07b97cdab84bf3280fa7a253e105
Auxile_Isaac <auxile.isaac@gmail.com>
2022-10-14 21:13:43 +0200

    correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# Creates a file
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
+# Create a file in /tmp using Puppet
+
+file { 'holberton':
+  ensure  => present,
+  path    => '/tmp/holberton',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  mode    => '0744',
+  content => 'I love Puppet',
 }
